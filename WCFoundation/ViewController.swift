4f51d1540ace09924d5dd2a8e3f46c70acd8c360
Frank Huang <wchuang@outlook.com>
2019-09-14 17:21:56 +0800

    Initial Commit

---
+++ WCFoundation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WCFoundation
+//
+//  Created by Frank on 2019/9/14.
+//  Copyright © 2019 Frank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
